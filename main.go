b735992a5544c998f2c5758bddf52eb7a61d13ef
rifaiAhmed <rifaiahmed078@gmail.com>
2025-10-01 23:18:20 +0700

    ok

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	log.Println("listening on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	log.Println("listening on port 8081...")
+	http.ListenAndServe(":8081", nil)
 }
